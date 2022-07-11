105145adf740a653fc205345d40d72160d54f140
Aryan Raj <ar9131000@gmail.com>
2022-07-11 17:59:50 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://aryan-alb-1714603034.us-east-2.elb.amazonaws.com/';
+const API_ROOT = 'http://aryan-alb-1714603034.us-east-2.elb.amazonaws.com/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
